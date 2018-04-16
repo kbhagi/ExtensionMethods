51a87c33f70b3d07ec017f26679cc2fcdaf41635
bhargavak <bhargavak37@gmail.com>
2018-04-16 17:07:59 +0530

    Create ExtensionMethods.cs

---
+++ ExtensionMethods.cs
@@ -0,0 +1,12 @@
+# ExtensionMethods
+Extension Methods
+public static class DownloadManager
+{
+public static void Download(this Book book)
+{
+
+}
+}
+DownloadManager.Download(book);
+Download(book);
+book.Download();
